38ec79823e0f14d16e0b0c40ddcbb9a6258e8b87
capex122 <capex122@users.noreply.github.com>
2026-08-23 03:43:14 +0300

    fix: align MPVKit Swift platform requirements

--- src-tauri/plugins/harbor-mpv/ios/Package.swift
+++ src-tauri/plugins/harbor-mpv/ios/Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "tauri-plugin-harbor-mpv",
-    platforms: [.iOS(.v15)],
+    platforms: [.macOS(.v12), .iOS(.v15)],
     products: [.library(name: "tauri-plugin-harbor-mpv", type: .static, targets: ["tauri-plugin-harbor-mpv"])],
     dependencies: [
         .package(name: "Tauri", path: "../.tauri/tauri-api"),
